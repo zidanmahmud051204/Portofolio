43792ef97331222b624be8923ff56be809a89a44
zidanmahmud051204 <133644235+zidanmahmud051204@users.noreply.github.com>
2023-11-18 14:28:07 +0700

    update

--- main.js
+++ main.js
@@ -1,15 +1,22 @@
-function toggleDetail(e) {
-  const target = $(e.target);
-
-  const item = $(target).parents();
-
-  const detail = $(item).children('.exp-detail');
-
-  $(detail).slideToggle();
-
-  if ($(target).hasClass('active')) {
-    $(target).html('Info Detail').removeClass('active');
+function valdiationForm(e) {
+  e.preventDefault();
+  const email = $('#inp-email');
+  const nama = $('#inp-nama');
+  const phone = $('#inp-phone-number');
+  const message = $('#inp-message');
+  if (!$(email).val()) {
+    alert('Email Wajib Diisi');
+  } else if (!$(nama).val()) {
+    alert('Nama Wajib Di isi');
+  } else if (!$(phone).val()) {
+    alert('Nomor Hp Wajib Di isi');
+  } else if (!$(message).val()) {
+    alert('Pesan Wajib Diisi');
   } else {
-    $(target).html('Baca Info').addClass('active');
+    $(nama).val('');
+    $(email).val('');
+    $(phone).val('');
+    $(message).val('');
+    alert('Berhasil Dikirim');
   }
 }
